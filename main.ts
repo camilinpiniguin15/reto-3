f67d423ddee58f4f86236e6a442e856d19df291f
camilinpiniguin15 <137525905+camilinpiniguin15@users.noreply.github.com>
2023-06-23 10:44:47 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,5 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("la temperatura actual es")
+    basic.showNumber(input.temperature())
+    basic.showString("grados centigrados")
+})
